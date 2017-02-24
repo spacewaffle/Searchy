ebcab738f1c41f03ab86e805ba3658187111e244
Jon Cheng <jon.syndicate@gmail.com>
2017-02-24 13:38:11 -0500

    first rx commit

--- Searchy/AppDelegate.swift
+++ Searchy/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        
+        let viewController = ViewController(nibName: "View", bundle: nil)
+        
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
